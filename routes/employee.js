8ab07d80152e17383924a51ad611dcca6a1b283a
oscillate123 <neonode2014@gmail.com>
2019-08-24 23:47:15 +0200

    big change from Jontis

--- routes/employee.js
+++ routes/employee.js
@@ -65,23 +65,26 @@ module.exports = {
     viewEmployeePage: asyncHandler(async (req, res) => {
         let userId = req.params.id;
 
-        
-        let employeeQuery = "SELECT * FROM biostar2_ac.t_usr WHERE USRUID = '" + userId + "' ";
-        let employeeResult = await db.query(employeeQuery);
+        try{
+            let employeeQuery = "SELECT * FROM biostar2_ac.t_usr WHERE USRUID = '" + userId + "' ";
+            let employeeResult = await db.query(employeeQuery);
 
-        let CFtypeQuery = "SELECT * FROM biostar2_ac.t_usrcusfld WHERE USRUID = '" + employeeResult[0].USRUID + "' and CUSFLDUID = '1'"; 
-        // biostar2_ac.t_cufldtyp & t_usrcusfld
-        let CFResult = await db.query(CFtypeQuery);
+            let CFtypeQuery = "SELECT * FROM biostar2_ac.t_usrcusfld WHERE USRUID = '" + employeeResult[0].USRUID + "' and CUSFLDUID = '1'"; 
+            // biostar2_ac.t_cufldtyp & t_usrcusfld
+            let CFResult = await db.query(CFtypeQuery);
 
-        let timecardQuery = "SELECT * FROM biostar_tna.timecard WHERE user_id = " + employeeResult[0].USRID + ";";
-        let timecardResult = await db.query(timecardQuery);
+            let timecardQuery = "SELECT * FROM biostar_tna.timecard WHERE user_id = " + employeeResult[0].USRID + ";";
+            let timecardResult = await db.query(timecardQuery);
 
-        res.render('view-employee.ejs', {
-            employee: employeeResult[0],
-            timecard: timecardResult,
-            personnummer: CFResult.length ? CFResult[0].VAL : 'N/A',
-            moment: moment,
-        });
+            res.render('view-employee.ejs', {
+                employee: employeeResult[0],
+                timecard: timecardResult,
+                personnummer: CFResult.length ? CFResult[0].VAL : 'N/A',
+                moment: moment,
+            });
+        }catch(e){
+            res.redirect('/');
+        }
     }),
     editUser: (req, res) => {
         let userId = req.params.id;
