e3aa1ef791104d9d15001cc177b4e45e07dcc97e
oscillate123 <neonode2014@gmail.com>
2019-08-24 23:22:40 +0200

    crazy idea is working!!!!

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,31 @@ const moment = require('moment');
 module.exports = {
     getHomePage: (req, res) => {
         let employeesListQuery = "SELECT * FROM biostar2_ac.t_usr order by NM asc;"; // query database to get all the Users
+        let CFtypeQuery = "SELECT * FROM biostar2_ac.t_usrcusfld WHERE CUSFLDUID = '1'";
 
         // execute query
         db.query(employeesListQuery, (err, result) => {
             if (err) {
                 res.redirect('/');
-            }
-            res.render('index.ejs', {
-                employees: result,
-                moment: moment,
+            } 
+            db.query(CFtypeQuery, (CFerr, CFResult) => {
+                if (CFerr) {
+                    res.redirect('/');
+                } 
+                let personnummer = {};
+                CFResult.forEach((cf) => {
+                    personnummer[cf.USRUID] = cf.VAL;
+                    });
+                    res.render('index.ejs', {
+                        employees: result,
+                        moment: moment,
+                        personnummer: personnummer,
+                });
             });
         });
     },
 };
+
+
+
+
