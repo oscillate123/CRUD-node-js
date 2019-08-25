b51966bc4fdfd415aff71aed90c1c3a4c274e15e
oscillate123 <neonode2014@gmail.com>
2019-08-25 15:35:17 +0200

    added asyncHandler module for index.js and adjusted the code for that module

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,31 @@
 const moment = require('moment');
+const asyncHandler = require('express-async-handler');
 
 module.exports = {
-    getHomePage: (req, res) => {
-        let employeesListQuery = "SELECT * FROM biostar2_ac.t_usr order by NM asc;"; // query database to get all the Users
-        let CFtypeQuery = "SELECT * FROM biostar2_ac.t_usrcusfld WHERE CUSFLDUID = '1'";
+    getHomePage: asyncHandler(async (req, res) => {
+        try{
+            let employeesListQuery = "SELECT * FROM biostar2_ac.t_usr order by NM asc;"; // query database to get all the Users
+            let emplResult = await db.query(employeesListQuery);
 
-        // execute query
-        db.query(employeesListQuery, (err, result) => {
-            if (err) {
-                res.redirect('/');
-            } 
-            db.query(CFtypeQuery, (CFerr, CFResult) => {
-                if (CFerr) {
-                    res.redirect('/');
-                } 
-                let personnummer = {};
-                CFResult.forEach((cf) => {
-                    personnummer[cf.USRUID] = cf.VAL;
-                    });
-                    res.render('index.ejs', {
-                        employees: result,
-                        moment: moment,
-                        personnummer: personnummer,
-                });
+            let CFtypeQuery = "SELECT * FROM biostar2_ac.t_usrcusfld WHERE CUSFLDUID = '1'";
+            let CFResult = await db.query(CFtypeQuery);
+
+            let personnummer = {};
+            CFResult.forEach((cf) => {
+                personnummer[cf.USRUID] = cf.VAL;
+            }),
+
+            res.render('index.ejs', {
+                employees: emplResult,
+                moment: moment,
+                personnummer: personnummer,
             });
-        });
-    },
+        }catch(e){
+            res.redirect('/');
+        };
+    }),
 };
+
 
 
 
